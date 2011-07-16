f34b567a631e3fe7c48d22d43c77d3d121c3b04e
Jaline Sousa <jaline.sousa@gmail.com>
2011-07-15 22:07:29 -0300

    classe teste artista

--- artista.py
+++
@@ -1,9 +0,0 @@
-#coding: utf -8
-
-class Artista(object):
-
-    def __init__(self, nome, pais, data_nascimento):
-        self.nome=nome
-        self.pais=pais
-        self.data_nascimento=data_nascimento
-    
