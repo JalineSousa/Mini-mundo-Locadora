6a81edfd2ef984e6cdddecdc1fd02298a9d2c5c8
Jaline Sousa <jaline.sousa@gmail.com>
2011-07-15 22:24:37 -0300

    classe artista

---
+++ artista.py
@@ -0,0 +1,9 @@
+#coding: utf -8
+
+class Artista(object):
+
+    def __init__(self, nome, pais, data_nascimento):
+        self.nome=nome
+        self.pais=pais
+        self.data_nascimento=data_nascimento
+    
